93c02b562a02e0ea8cba9996d8202736f90036e2
HirschBerge <hskirkwo@geneva.edu>
2023-04-23 18:49:12 -0400

    Added a setup.sh script to help the setup process on new systems.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+bin_path='/usr/local/bin'
+sudo ln -s $PWD/animewget.py "$bin_path/animewget"
+sudo ln -s $PWD/mangadex.py "$bin_path/mdex"
+sudo ln -s $PWD/mounting "$bin_path/"
+sudo ln -s $PWD/yt "$bin_path/" 
+sudo ln -s $PWD/dudu "$bin_path/"
+sudo ln -s $PWD/pacUpdate.sh "$bin_path/pacUpdate"

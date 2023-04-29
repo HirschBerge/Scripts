3879e1e9c6ac85dc5f7b8bdc4841e3ac8ec0c2d3
HirschBerge <hskirkwo@geneva.edu>
2023-04-29 19:59:18 -0400

    Added yt.sh back

---
+++ yt.sh
@@ -0,0 +1,15 @@
+#!/bin/env sh
+
+clip=$(xclip -selection c -o)
+echo $clip
+if [[ $1 == *"http"* ]]; then
+  url=$1
+elif [[ $clip == *"http"* ]]; then
+  url=$clip
+else
+  echo "No URL found"
+fi
+
+mpv --ao=pulse --no-config --script-opts=ytdl_hook-ytdl_path=yt-dlp --msg-level=all=no,ytdl_hook=trace --fs=yes $url >/dev/null 2>&1 &
+clear
+exit

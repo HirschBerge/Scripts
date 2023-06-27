3502d9ecf28e4718e09991e6834cc6a99fea4abb
HirschBerge <hskirkwo@geneva.edu>
2023-06-27 19:31:38 -0400

    Added python dice roller

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 bin_path='/usr/local/bin'
 chmod +x -R ~/.scripts/
-sudo ln -s $PWD/animewget.py "$bin_path/animewget"
-sudo ln -s $PWD/mangadex.py "$bin_path/mdex"
-sudo ln -s $PWD/mounting "$bin_path/mounting"
-sudo ln -s $PWD/yt.sh "$bin_path/yt" 
-sudo ln -s $PWD/web.sh "$bin_path/web" 
-sudo ln -s $PWD/dudu.sh "$bin_path/dudu"
-sudo ln -s $PWD/pacUpdate.sh "$bin_path/pacUpdate"
+sudo ln -s ~/.scripts/animewget.py "$bin_path/animewget"
+sudo ln -s ~/.scripts/mangadex.py "$bin_path/mdex"
+sudo ln -s ~/.scripts/mounting "$bin_path/mounting"
+sudo ln -s ~/.scripts/yt.sh "$bin_path/yt" 
+sudo ln -s ~/.scripts/web.sh "$bin_path/web" 
+sudo ln -s ~/.scripts/dudu.sh "$bin_path/dudu"
+sudo ln -s ~/.scripts/pacUpdate.sh "$bin_path/pacUpdate"

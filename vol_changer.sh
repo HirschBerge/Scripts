3f2d9caa9e2a928029ecd4c2f64a7b2c46eed805
Hirschy <hskirkwo@geneva.edu>
2023-07-02 21:19:03 -0400

    A few more minor tweaks

--- vol_changer.sh
+++ vol_changer.sh
@@ -2,4 +2,4 @@
 
 vol_device=$(wpctl status |grep Sinks -m1 -A 13 |grep -i "vol:" -m 1 |awk -F"*" '{ print $2 }' |awk '{ print $1 }' |sed 's/\.//g')
 echo $1
-wpctl set-volume $vol_device "$1"
+[[ "$1" -eq "toggle" ]] && wpctl set-mute $vol_device toggle || wpctl set-volume $vol_device "$1"

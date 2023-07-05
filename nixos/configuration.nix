d81dcf95d95172e19b274a146a8952895b3c26c0
Hirschy <hskirkwo@geneva.edu>
2023-07-05 01:58:15 -0400

    Added Brave extensions

---
+++ nixos/configuration.nix
@@ -0,0 +1,184 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
+
+{
+  # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
+  #sound.enable = false;
+  systemd.services.background = {
+    script = ''
+      sleep 15; /home/hirschy/.scripts/background/cron.sh /home/hirschy/Pictures/Sci-Fi/ 
+    '';
+    wantedBy = [ "multi-user.target" ];
+  };
+
+
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
+  imports =
+    [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+        <home-manager/nixos>
+    ];
+
+  environment.pathsToLink = [ "/libexec"];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "yoitsu"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+  # services.xserver.windowManager.i3.package = pkgs.i3-gaps;
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbVariant = "";
+    
+    desktopManager = {
+     xterm.enable = false;
+     };
+    displayManager = {
+      defaultSession = "none+i3";
+      };
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        dmenu
+      	i3status
+      	i3lock
+      	i3blocks
+      	polybar
+        siji
+      ];
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.hirschy = {
+    isNormalUser = true;
+    description = "Hirschy";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.hirschy = import ./home.nix;
+  };
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "openssl-1.1.1u"
+  ];
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    brave
+    git
+    home-manager
+    sublime4
+    python311
+    python311Packages.pip
+    pavucontrol
+    autojump
+    discord
+    spotify
+    steam
+    picom
+    sweet
+    mpv
+    yt-dlp
+    chromium
+    ripgrep
+    cmake
+    lm_sensors
+    zip
+    bottles
+    steam-run
+    lutris
+    wineWowPackages.full
+  ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";    
+    };
+  };
+  # List services that you want to enable:
+  services.dbus.packages = [
+    pkgs.dbus.out
+    config.system.path
+  ];
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+  services.xserver.windowManager.i3.package = pkgs.i3-gaps;
+
+}
---
+++ nixos/home.nix
@@ -0,0 +1,65 @@
+{config, pkgs, ...}:
+
+{
+	imports = [ ./configs/zsh.nix ./configs/i3.nix ./configs/kitty.nix ./configs/sxhkd.nix ./configs/polybar.nix ./configs/starship.nix];
+	home.username = "hirschy";
+	home.homeDirectory = "/home/hirschy";
+	home.stateVersion = "23.05";
+	home.packages = with pkgs; [
+		zsh
+		htop
+		i3-gaps
+		rofi
+		kitty
+		starship
+		fzf
+        sxhkd
+	    bat
+	    xclip
+	    axel
+		ranger
+    	xwallpaper
+        exa
+        nerdfonts
+	    unzip
+	    sxiv
+	    maim
+	    betterdiscordctl
+	    xcape
+	    xorg.xmodmap
+	    xdotool
+	    nmap
+	    grc
+	    i3lock-fancy-rapid
+	    neofetch
+	];
+	programs.fzf = {
+		enable = true;
+		enableZshIntegration = true;
+	};
+	programs.chromium = {
+		enable = true;
+		extensions = [
+			{ id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+			{ id = "aapbdbdomjkkjkaonfhkkikfgjllcleb";} # Google Translate
+			{ id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";} # Dark Reader
+			{ id = "bmnlcjabgnpnenekpadlanbbkooimhnj";} # Honey
+			{ id = "hdokiejnpimakedhajhdlcegeplioahd";} # Last Pass
+			{ id = "ponfpcnoihfmfllpaingbgckeeldkhle";} # Enhancer for Youtube
+			{ id = "kbfnbcaeplbcioakkpcpgfkobkghlhen";} # Grammarly
+		];
+	};
+	programs.brave = {
+		enable = true;
+		extensions = [
+			{ id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
+			{ id = "aapbdbdomjkkjkaonfhkkikfgjllcleb";} # Google Translate
+			{ id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";} # Dark Reader
+			{ id = "bmnlcjabgnpnenekpadlanbbkooimhnj";} # Honey
+			{ id = "hdokiejnpimakedhajhdlcegeplioahd";} # Last Pass
+			{ id = "ponfpcnoihfmfllpaingbgckeeldkhle";} # Enhancer for Youtube
+			{ id = "kbfnbcaeplbcioakkpcpgfkobkghlhen";} # Grammarly
+		];
+	};
+}
+

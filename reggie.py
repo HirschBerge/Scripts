8da110a136e4dd8182b43cf9040e563c5f663c52
Hirschy <53668025+HirschBerge@users.noreply.github.com>
2023-07-06 15:24:37 -0400

    Add files via upload

--- reggie.py
+++ reggie.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import re
-
+# This script is to partially automate the process of porting a traditional polybar config for use with NixOS home-manager. 
+# it should still be inspected and any necessary changes made.
 in_file = "./modules.ini"
 out_file = "./modules.nix"
 
@@ -10,11 +11,11 @@ def main():
         with open(out_file, "w+") as outf:
             for line in file:
                 line = line.strip()
-                line = re.sub(';', '#', line)
+                line = re.sub(r'^\s*;', '#', line)
                 line = re.sub(r'=$', '= ', line)
                 search_pattern_1 = r'= '
                 search_pattern_2 = r'\[([bar module].*?)\]'
-                if not re.search(r'^\s.*#', line):
+                if not re.search(r'^\s*#', line):
                     if re.search(search_pattern_1, line):
                         line = re.sub(r"$", '";', line)
                         line = re.sub('= ', '= "', line)
@@ -27,6 +28,7 @@ def main():
                             line = re.sub(r'\]', '" = {', line)
                         count += 1
                 if not (re.match(r'^\s*$', line)):
+                    line = re.sub('""', '"', line)
                     print(line)
                     outf.write(f"{line}\n")
 

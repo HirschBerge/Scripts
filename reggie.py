85543a7b6975bc269e8ecc0ffd0681389ac08951
Hirschy <53668025+HirschBerge@users.noreply.github.com>
2023-07-06 13:22:05 -0400

    additional changes to reggie.py

--- reggie.py
+++ reggie.py
@@ -5,23 +5,30 @@ in_file = "./modules.ini"
 out_file = "./modules.nix"
 
 def main():
+    count = 0
     with open(in_file, "r") as file:
         with open(out_file, "w+") as outf:
             for line in file:
                 line = line.strip()
+                line = re.sub(r'=$', '= ', line)
                 search_pattern_1 = r'= '
-                search_pattern_2 = r'\[([bar|module].*?)\]'
+                search_pattern_2 = r'\[([bar module].*?)\]'
                 line = re.sub(';', '#', line)
-                if re.search(search_pattern_1, line):
-                    line = re.sub(r"$", '";', line)
-                    line = re.sub('= ', '= "', line)
-                    line = re.sub(r'^', r'\t', line)
-                    # print(line)
-                if re.search(search_pattern_2, line):
-                    line = re.sub(r'\[', '};\n"', line)
-                    line = re.sub(r'\]', '" = {', line)
-                print(line)
-                outf.write(f"{line}\n")
+                if "#" not in line:
+                    if re.search(search_pattern_1, line):
+                        line = re.sub(r"$", '";', line)
+                        line = re.sub('= ', '= "', line)
+                        line = re.sub(r'^', r'\t', line)
+                        # print(line)
+                    if re.search(search_pattern_2, line):
+                        # print(line)
+                        if count >= 1:
+                            line = re.sub(r'\[', '};\n"', line)
+                            line = re.sub(r'\]', '" = {', line)
+                        count += 1
+                if not (re.match(r'^\s*$', line)):
+                    print(line)
+                    outf.write(f"{line}\n")
 
 if __name__=='__main__':
     main()

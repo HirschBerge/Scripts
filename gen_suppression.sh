15fa4063069b2d7224ec58c02b6e963c64600323
Hirschy <hskirkwo@geneva.edu>
2023-07-09 02:23:33 -0400

    Added existence checking.

--- gen_suppression.sh
+++ gen_suppression.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+
+NoColor="\033[0m"
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+file_path="/home/hirschy/.config/pipewire/pipewire.conf.d/"
+[ -d "$file_path" ] || mkdir -p $file_path
+[ -f "$file_path/99-input-denoising" ] || touch "$file_path/99-input-denoising.so"
 pkg_path="$(nix-store --query --requisites /run/current-system |grep -m 1 rnnoise)/lib/ladspa/librnnoise_ladspa.so"
 echo -en "
 context.modules = [
@@ -34,15 +48,7 @@ context.modules = [
     }
 }
 ]
-" > ~/.config/pipewire/pipewire.conf.d/99-input-denoising.conf
-NoColor="\033[0m"
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
+" > "$file_path/99-input-denoising.so"
 
-printf "${Green}[+]${NoColor} Completed generating ${Purple} ~/.config/pipewire/pipewire.config.d/99-input-denoising${NoColor}"
+
+printf "${Green}[+]${NoColor} Completed generating ${Purple} ~/.config/pipewire/pipewire.config.d/99-input-denoising.so${NoColor}"

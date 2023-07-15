7a74672d58cb5b595325b5948ad4acdd5344f404
Hirschy <hskirkwo@geneva.edu>
2023-07-15 19:34:38 -0400

    Changed coloring as polybar doesn't support the printf statements

--- nvidia_mon_poly.sh
+++ nvidia_mon_poly.sh
@@ -1,14 +1,14 @@
 
-# Bold High Intensity
-NoColor="\033[0m"
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
+# # Bold High Intensity
+# NoColor="\033[0m"
+# BIBlack='\033[1;90m'      # Black
+# BIRed='\033[1;91m'        # Red
+# %{F#00FF00}='\033[1;92m'      # Green
+# BIYellow='\033[1;93m'     # Yellow
+# BIBlue='\033[1;94m'       # Blue
+# BIPurple='\033[1;95m'     # Purple
+# BICyan='\033[1;96m'       # Cyan
+# BIWhite='\033[1;97m'      # White
 
 output=$(nvidia-smi --query-gpu=memory.used,utilization.gpu,temperature.gpu --format=csv,noheader,nounits | awk -F"," '{ print $1 $2 $3}')
 # echo $output
@@ -19,46 +19,46 @@ temp=$(echo $output |awk '{ print $3 }')
 function color_mem() {
 	if [ $mem -le "1000" ]
 	then
-		mem="$BIGreen$mem$NoColor"
+		mem="$%{F#00FF00}$mem"
 	elif [ $mem -gt "1000" ] && [ $mem -le "3000" ]
 	then
-		mem="$BIPurple$mem$NoColor"
+		mem="%{F#5D3FD3}$mem"
 	elif [ $mem -gt "3001" ] && [ $mem -le "6000" ]
 	then
-		mem="$BIYellow$mem$NoColor"
+		mem="%{F#ffff00}$mem"
 	else
-		mem="$BIRed$mem$NoColor"
+		mem="%{F#FF0000}$mem"
 	fi
 }
 function color_util() {
 	if [ $util -le "30" ]
 	then
-		util="$BIGreen$util$NoColor"
+		util="$%{F#00FF00}$util"
 	elif [ $util -gt "30" ] && [ $util -le "70" ]
 	then
-		util="$BIPurple$util$NoColor"
+		util="%{F#5D3FD3}$util"
 	elif [ $util -gt "70" ] && [ $util -le "85" ]
 	then
-		util="$BIYellow$util$NoColor"
+		util="%{F#ffff00}$util"
 	else
-		util="$BIRed$util$NoColor"
+		util="%{F#FF0000}$util"
 	fi
 }
 function color_temp() {
 	if [ $temp -le "40" ]
 	then
-		temp="$BIGreen$temp$NoColor"
+		temp="%{F#00FF00}$temp"
 	elif [ $temp -gt "40" ] && [ $temp -le "69" ]
 	then
-		temp="$BIPurple$temp$NoColor"
+		temp="%{F#5D3FD3}$temp"
 	elif [ $temp -gt "70" ] && [ $temp -le "79" ]
 	then
-		temp="$BIYellow$temp$NoColor"
+		temp="%{F#ffff00}$temp"
 	else
-		temp="$BIRed$temp$NoColor"
+		temp="%{F#FF0000}$temp"
 	fi
 }
 color_mem
 color_temp
 color_util
-printf "${BICyan}RTX 3060ti${NoColor} ${mem}Mb ${util}%% ${temp}°C"
+echo "%{F#00ffff}RTX 3060ti ${mem}Mb ${util}% ${temp}°C"

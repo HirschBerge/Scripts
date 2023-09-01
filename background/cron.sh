de75dd0283feb5d34533bb5b2fc7a6fee1bfe76a
Hirschy <hskirkwo@geneva.edu>
2023-08-31 20:37:30 -0400

    Hehe

--- background/cron.sh
+++ background/cron.sh
@@ -6,6 +6,19 @@ if [ -n "$1" ]; then
   folder="$1"
 fi
 
+send_discord_message() {
+  local WEBHOOK_URL="https://discord.com/api/webhooks/1085380232908902420/67yS35DSklhRDQyf9r-9om4ragu6rpXykkQeXoXoRpa5ACcCeNftme_QxMnbzdUDhatO"
+  local USERNAME="Sexy Surprise"
+  local message_content="$1"
+
+  curl -H "Content-Type: application/json" -X POST -d '{
+    "content": "'"${message_content}"'",
+    "username": "'"${USERNAME}"'"
+  }' "${WEBHOOK_URL}"
+}
+
+
+
 shufflezwide2() {
   for z in $(/etc/profiles/per-user/hirschy/bin/exa -d "$folder"/* | shuf | head -1); do
     echo "$z" > "$HOME/.scripts/background/resourcesnorm"
@@ -36,7 +49,7 @@ sets_background() {
   echo "$imgnorm" "$imgwide"
   cp "$imgwide" ~/.config/wallwide.png
   cp "$imgnorm" ~/.config/wallwide2.png
-  [[ $number -eq 10 ]] && /run/wrappers/bin/sudo -u hirschy DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path/run/user/1000/bus /etc/profiles/per-user/hirschy/bin/xwallpaper --no-randr --focus /mnt/NAS/Pictures/lewd2.png || /run/wrappers/bin/sudo -u hirschy DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path/run/user/1000/bus /etc/profiles/per-user/hirschy/bin/xwallpaper --output DP-2 --zoom ~/.config/wallwide.png --output DP-0 --zoom ~/.config/wallwide2.png
+  [[ $number -eq 10 ]] && {/run/wrappers/bin/sudo -u hirschy DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path/run/user/1000/bus /etc/profiles/per-user/hirschy/bin/xwallpaper --no-randr --focus /mnt/NAS/Pictures/lewd2.png ; send_discord_message "<@215327353423921159> SURPRISEEEEE"} || /run/wrappers/bin/sudo -u hirschy DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path/run/user/1000/bus /etc/profiles/per-user/hirschy/bin/xwallpaper --output DP-2 --zoom ~/.config/wallwide.png --output DP-0 --zoom ~/.config/wallwide2.png
 }
 
 main() {

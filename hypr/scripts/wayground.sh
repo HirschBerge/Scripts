21cc9aa32841c480c470cef6c22174ddb7a10b7b
HirschBerge <hskirkwo@geneva.edu>
2023-10-14 17:19:37 -0400

    Some updates.

--- hypr/scripts/wayground.sh
+++ hypr/scripts/wayground.sh
@@ -3,5 +3,5 @@
 opt1=$(shuf -n 1 -e ~/Pictures/Monogatari/*)
 opt2=$(shuf -n 1 -e ~/Pictures/Monogatari/*)
 
-/run/current-system/sw/bin/swww img -t random -o DP-1 $opt1
-/run/current-system/sw/bin/swww img -t random -o DP-2 $opt2
+swww img -t random -o DP-1 $opt1
+swww img -t random -o DP-2 $opt2

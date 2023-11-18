58a398b18b80e5522f83436a2446bd7742fa818a
HirschBerge <hskirkwo@geneva.edu>
2023-11-17 23:50:49 -0500

    Better Blur

--- nixos/configs/kitty.nix
+++ nixos/configs/kitty.nix
@@ -21,9 +21,9 @@ with colors; {
 						bold_italic_font Fira Code SemiBold Nerd Font Complete
 						symbol_map U+1F980 Noto Color Emoji
 						 enable_audio_bell no
-						background_opacity 0.75
-						 background_tint 0.7
-						 dim_opacity 0.75
+						background_opacity 0.6
+						 background_tint 0.6
+						 dim_opacity 0.6
 						shell zsh
 						map ctrl+shift+enter	launch	--cwd=current --type=os-window
     '';#I honestly have no clue why, but passing these options as settings {}; 

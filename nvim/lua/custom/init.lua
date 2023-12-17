93eb78cfd21ff36f2b8a8459211e040bbea2db5a
HirschBerge <hskirkwo@geneva.edu>
2023-12-16 21:00:04 -0500

    Added autocmds to move cursor to last location

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -4,3 +4,4 @@ vim.cmd('source ' .. vimrc)
 vim.opt.scrolloff = 20
 vim.keymap.set('n', 'j', "jzz", {})
 vim.keymap.set('n', 'j', "kzz", {})
+vim.cmd('source ' .. "~/.config/nvim/lua/custom/autocmds.lua")

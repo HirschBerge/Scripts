a851a9f223b6ecf4778a0c3124320979871b2286
HirschBerge <hskirkwo@geneva.edu>
2023-12-26 21:44:20 -0500

    Fixed update script!

--- update_conf.sh
+++ update_conf.sh
@@ -5,20 +5,20 @@ BCyan='\033[1;36m'
 NoColor="\033[0m"
 BGreen='\033[1;32m'
 dots="$(dirname "$(readlink -f "$0")")"
-for i in "$dots"/*/; 
+for i in "$dots/$fixed"/*/; 
 do 
     fixed=$(basename "$i")
     if [ "$fixed" == "nixos" ]
     then
-      printf "${BGreen}[+] ${BYellow}Copying ${BPurple}/etc/$fixed ${BYellow}to ${BCyan}$dots${NoColor}\n"
+      printf "${BGreen}[+] ${BYellow}Copying ${BPurple}/etc/$fixed ${BYellow}to ${BCyan}$dots/$fixed${NoColor}\n"
       rsync -rah --progress -i /etc/nixos/* nixos/ >/dev/null
     elif
       [ "$fixed" == "mozilla" ]
     then
-      printf "${BGreen}[+] ${BYellow}Copying ${BPurple}$HOME/.$fixed ${BYellow}to ${BCyan}$dots${NoColor}\n"
+      printf "${BGreen}[+] ${BYellow}Copying ${BPurple}$HOME/.$fixed ${BYellow}to ${BCyan}$dots/$fixed${NoColor}\n"
       rsync -rah --progress -i "$HOME/.$fixed/" "$dots/$fixed" >/dev/null
     else
-      printf "${BGreen}[+] ${BYellow}Copying ${BPurple}$HOME/.config/$fixed ${BYellow}to ${BCyan}$dots${NoColor}\n"
+      printf "${BGreen}[+] ${BYellow}Copying ${BPurple}$HOME/.config/$fixed ${BYellow}to ${BCyan}$dots/$fixed${NoColor}\n"
       rsync -rah --progress -i "$HOME/.config/$fixed/" "$dots/$fixed/" >/dev/null
     fi
 done

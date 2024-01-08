8ebb333be14b32f3b6bae85832dd477453233b7f
HirschBerge <hskirkwo@geneva.edu>
2024-01-08 15:42:23 -0500

    idk anymore

--- flake.nix
+++ flake.nix
@@ -4,19 +4,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    firefox-addons = { url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: 
     let 
-      system = "x86_64-linux";
-      firefox-addons = { url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons"; inputs.nixpkgs.follows = "nixpkgs"; };
-
-      pkgs = import nixpkgs {
-        inherit system;
-
-        config = {
-          allowUnfree = true;
-        };
+      system = "x86_64-linux"; 
+      pkgs = import nixpkgs { 
+        system = "x86_64-linux"; 
+        config.allowUnfree = true; 
       };
     in
     {
@@ -24,7 +20,7 @@
       yoitsu = nixpkgs.lib.nixosSystem {
         specialArgs = {
           inherit system;
-          inherit inputs;
+          # inherit inputs;
         };
         system = "x86_64-linux";
         modules = [
@@ -35,10 +31,9 @@
             home-manager.useUserPackages = true;
             home-manager.users.hirschy = import ./nixos/home.nix;
             home-manager.extraSpecialArgs = {
-              inherit inputs;
+              inherit inputs; 
+              inherit system;
             };
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
           }
         ];
       };

2fb3896a7a88f56b24e4b735634bd6bc8e59065b
Berge della Hirsch <53668025+HirschBerge@users.noreply.github.com>
2024-01-09 23:04:23 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "My Favorite NixOS flake!";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+         url = "github:nix-community/home-manager";
+         inputs.nixpkgs.follows = "nixpkgs";
+    };
     nur.url = "github:nix-community/nur";
   };
 

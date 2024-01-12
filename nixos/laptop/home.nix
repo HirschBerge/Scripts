2303c2b06bb191048d27182f60f42a52877a27d7
HirschBerge <hskirkwo@geneva.edu>
2024-01-11 20:17:00 -0500

    Moved laptop over to Hyprland flake and config'd via HM

--- nixos/laptop/home.nix
+++ nixos/laptop/home.nix
@@ -208,6 +208,48 @@ in
  #            { id = "mnjggcdmjocbbbhaepdhchncahnbgone";} # Sponsorblock
 	# 	];
 	# };
-	
+  services.swayidle =
+    let
+      lockCommand = "${pkgs.swaylock-effects}/bin/swaylock --screenshots --clock --indicator --indicator-radius 100 --indicator-thickness 7 --effect-blur 7x5 --effect-vignette 0.5:0.5 --ring-color bb00cc --key-hl-color 880033 --line-color 00000000 --inside-color 00000088 --separator-color 00000000 --grace 2 --fade-in 0.2";
+    in
+    {
+      enable = true;
+      systemdTarget = "hyprland-session.target";
+      timeouts =
+        let
+          dpmsCommand = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/hyprctl dispatch dpms";
+        in
+        [
+          {
+            # timeout = 300;
+            timeout = 120;
+            command = lockCommand;
+          }
+          {
+            # timeout = 600;
+            timeout = 150;
+            command = "${dpmsCommand} off";
+            resumeCommand = "${dpmsCommand} on";
+          }
+          {
+            timeout = 180;
+            command = "${pkgs.systemctl}/bin/systemctl hibernate"
+          }
+        ];
+      events = [
+        {
+          event = "before-sleep";
+          command = lockCommand;
+        }
+        {
+          event = "lock";
+          command = lockCommand;
+        }
+        {
+          event = "after-resume";
+          command = "${inputs.hyprland.packages.${pkgs.system}.hyprland}/bin/hyprctl dispatch dpms";
+        }
+      ];
+    };
 }
 
